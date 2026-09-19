058860a11cc2dd04f79ba943dadad507a7ae900b
Sol Sopheak <sopheaksol71@gmail.com>
2026-09-19 15:41:50 +0700

    Project : microservice

---
+++ task-service/src/test/java/com/sopheak/microservices/task_service/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopheak.microservices.task_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
